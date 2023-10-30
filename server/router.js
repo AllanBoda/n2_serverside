e00459a922ec234520e0ae9bdf3cc068fbc9d16f
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2023-10-30 20:04:23 -0300

    Update router.js

--- server/router.js
+++ server/router.js
@@ -4,6 +4,7 @@ const { prod } = require("../client/client.js");
 let router = express.Router();
 
 router.get('/prod', prod.all);
+router.get('/end/:cod', prod.end_point);
 router.post('/prod', prod.create);
 router.put('/prod/:cod', prod.update);
 router.delete('/prod/:cod', prod.delete);
