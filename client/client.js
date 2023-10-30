7ed2cd8c44314f509c481432e5b42cd0914397b7
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2023-10-30 19:04:04 -0300

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,59 +1,92 @@
 const { connection } = require("./database.js");
 let prod = {};
 
-prod.all = function (req, res){
-    try{
-        let prods =  connection.query("SELECT * FROM produtos;");
-        res.send(prods);
-    } catch(e){
-        console.log("Erro......", e)
-    }
-}
+prod.all = function (req, res) {
+  try {
+    connection.query("SELECT * FROM produtos", function (err, results) {
+      if (err) {
+        console.log("Erro: ", err);
+        res.status(500).json({ error: "Erro ao buscar produtos." });
+        return;
+      }
+
+      res.json(results);
+    });
+  } catch (e) {
+    console.log("Erro: ", e);
+    res.status(500).json({ error: "Erro ao buscar produtos." });
+  }
+};
 
 prod.create = function (req, res) {
-    try {
-        let prods = req.body;
-        let sql = "INSERT INTO produtos (cod_produto, nome_produto, preco_produto) VALUES (?,?,?);";
-        let values = [prods.cod_produto, prods.nome_produto, prods.preco_produto];
-        let result = connection.query(sql, values);
-        res.send({
-            status:"Inserção efetuada com sucesso!",
-            result: result
-        })
-    } catch (e) {
-        console.log("Erro:..........", e);
-    }
-}
+  try {
+    const prods = req.body;
+    const sql = "INSERT INTO produtos (nome_produto, preco_produto) VALUES (?, ?)";
+    const values = [prods.nome_produto, prods.preco_produto];
+
+    connection.query(sql, values, function (err, result) {
+      if (err) {
+        console.log("Erro: ", err);
+        res.status(500).json({ error: "Erro ao criar o produto." });
+        return;
+      }
+
+      res.json({
+        status: "Inserção efetuada com sucesso!",
+        result: result,
+      });
+    });
+  } catch (e) {
+    console.log("Erro: ", e);
+    res.status(500).json({ error: "Erro ao criar o produto." });
+  }
+};
 
 prod.update = function (req, res) {
-    try {
-        let cod = req.params.cod;
-        let prods = req.body;
-        let sql = "UPDATE produtos SET nome_produto=?, preco_produto=? WHERE cod_produto=?;";
-        let values = [prods.nome_produto, prods.preco_produto, cod];
-        let result = connection.query(sql, values);
-        res.send({
-            status:"Atualização do produto " + prods.nome_produto + " efetuada!",
-            result: result
-        })
-    } catch (e) {
-        console.log("Erro:..........", e)
-    }
-}
+  try {
+    const cod = req.params.cod;
+    const prods = req.body;
+    const sql = "UPDATE produtos SET nome_produto = ?, preco_produto = ? WHERE cod_produto = ?";
+    const values = [prods.nome_produto, prods.preco_produto, cod];
+
+    connection.query(sql, values, function (err, result) {
+      if (err) {
+        console.log("Erro: ", err);
+        res.status(500).json({ error: "Erro ao atualizar o produto." });
+        return;
+      }
+
+      res.json({
+        status: "Atualização do produto " + prods.nome_produto + " efetuada!",
+        result: result,
+      });
+    });
+  } catch (e) {
+    console.log("Erro: ", e);
+    res.status(500).json({ error: "Erro ao atualizar o produto." });
+  }
+};
 
 prod.delete = function (req, res) {
-    try {
-        let cod = req.params.cod;
-        let sql = "DELETE FROM produtos WHERE cod_produto=?;";
-        let result = connection.query(sql, cod);
-        res.send({
-            status:"Exclusão do produto " + cod + " foi efetuada!",
-            result: result
-        })
-    } catch (e) {
-        console.log("Erro:..........", e)
+  try {
+    const cod = req.params.cod;
+    const sql = "DELETE FROM produtos WHERE cod_produto = ?";
+    connection.query(sql, cod, function (err, result) {
+      if (err) {
+        console.log("Erro: ", err);
+        res.status(500).json({ error: "Erro ao excluir o produto." });
+        return;
+      }
 
-    }
-}
+      res.json({
+        status: "Exclusão do produto " + cod + " foi efetuada!",
+        result: result,
+      });
+    });
+  } catch (e) {
+    console.log("Erro: ", e);
+    res.status(500).json({ error: "Erro ao excluir o produto." });
+  }
+};
 
 module.exports = { prod };
