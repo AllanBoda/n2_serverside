59994b6feefbdfea6949dfaf0b15d0d08d9e2985
AllanBoda <111321006+AllanBoda@users.noreply.github.com>
2023-10-30 20:04:00 -0300

    Update client.js

--- client/client.js
+++ client/client.js
@@ -14,8 +14,30 @@ prod.all = function (req, res) {
     });
   } catch (e) {
     console.log("Erro: ", e);
-    res.status(500).json({ error: "Erro ao buscar produtos." });
+    res.status(500).json({ error: "Erro ao buscar os produtos." });
   }
+};
+
+prod.end_point = function (req, res){
+    try {
+    const cod = req.params.cod;
+    const sql = "SELECT * FROM produtos where preco_produto = ?;";
+    const values = [cod];
+
+    connection.query(sql, values,function (err, results) {
+            if (err) {
+              console.log("Erro: ", err);
+              res.status(500).json({ error: "Erro ao buscar os produtos." });
+              return;
+            }
+      
+            res.json(results);
+          });
+
+    } catch (e) {
+        console.log("Erro: ", e);
+        res.status(500).json({ error: "Erro ao buscar os produtos." });
+    }
 };
 
 prod.create = function (req, res) {
